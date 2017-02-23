09fb3be55aba5003ac346193e0a481b2c83d5e6f
jdollete <[01;31m[K"email"[m[K: "allain.dollete@gmail.com>
2017-02-22 21:37:52 -0500

    Create home controller and update routes

--- db/migrate/20170223013230_create_admins.rb
+++ db/migrate/20170223013230_create_admins.rb
@@ -1,8 +1,11 @@
 class CreateAdmins < ActiveRecord::Migration[5.0]
   def change
     create_table :admins do |t|
-
-      t.timestamps
+      t.string :username, null:false
+      t.string :password_digest, null:false
+      t.string :email, null:false
+      
+      t.timestamps(null:false)
     end
   end
 end
